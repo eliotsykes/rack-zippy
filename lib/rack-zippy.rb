6698950d7d601add1097f61b7ed41815e91e389b
Eliot Sykes <eliotsykes@gmail.com>
2015-10-11 19:09:31 +0100

    Default max_age_fallback to 1 day (in secs)

--- lib/rack-zippy.rb
+++ lib/rack-zippy.rb
@@ -10,14 +10,11 @@ module Rack
       # @param app [#call] the Rack app
       # @param path [String] the path to the public directory, usually where favicon.ico lives
       # @param max_age_fallback [Fixnum] optional time in seconds that Cache-Control header should use instead of the default
-      def initialize(app, path, max_age_fallback: nil)
+      def initialize(app, path, max_age_fallback: :day)
         assert_path_valid path
+        max_age_fallback = calc_max_age_fallback(max_age_fallback)
 
-        if max_age_fallback
-          cache_control = "public, max-age=#{max_age_fallback}"
-        else
-          cache_control = nil
-        end
+        cache_control = "public, max-age=#{max_age_fallback}"
 
         @static_middleware = ::ActionDispatch::Static.new(app, path, cache_control)
       end
@@ -34,6 +31,16 @@ module Rack
 
       BLANK_PATH_MESSAGE = 'Please specify non-blank path when initializing rack-zippy middleware ' +
         '(path leads to your public directory, often the one with favicon.ico in it)'
+
+      SECONDS_IN = {
+        :day => 24*60*60,
+        :month => 31*(24*60*60),
+        :year => 365*(24*60*60)
+      }.freeze
+
+      def calc_max_age_fallback(max_age_fallback)
+        max_age_fallback.is_a?(Symbol) ? SECONDS_IN.fetch(max_age_fallback) : max_age_fallback
+      end
 
       def assert_path_valid(path)
         raise ArgumentError, BLANK_PATH_MESSAGE if path.blank?
