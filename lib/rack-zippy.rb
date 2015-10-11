44b2e6a734d216698171bb16727d4ab384841c70
Eliot Sykes <eliotsykes@gmail.com>
2015-10-11 18:19:06 +0100

    attr_accessor for static middleware

--- lib/rack-zippy.rb
+++ lib/rack-zippy.rb
@@ -10,6 +10,8 @@ module Rack
       BLANK_PATH_MESSAGE = 'Please specify non-blank path when initializing rack-zippy middleware ' +
         '(path leads to your public directory, often the one with favicon.ico in it)'
 
+      attr_reader :static_middleware
+
       def initialize(app, path=nil, options={})
         raise ArgumentError, BLANK_PATH_MESSAGE if path.blank?
         @static_middleware = ::ActionDispatch::Static.new(app, path)
@@ -18,7 +20,7 @@ module Rack
       def call(env)
         return not_found_response if illegal_path?(env)
 
-        @static_middleware.call(env)
+        static_middleware.call(env)
       end
 
       private
