302c29d8e925c32fdc9810398615ede0c3d197a8
Eliot Sykes <eliotsykes@gmail.com>
2015-10-19 17:42:51 +0100

    Railtie test passing

--- lib/rack-zippy/railtie.rb
+++ lib/rack-zippy/railtie.rb
@@ -2,15 +2,21 @@ module Rack
   module Zippy
     class Railtie < ::Rails::Railtie
 
+      cattr_accessor :skip_version_check
+
       def self.version_check
-        # return if defined?(Rack::Zippy.skip_rails_version_check)
+        return if Rack::Zippy::Railtie.skip_version_check
         if ::Rails.version.to_f >= minimum_rails_version_with_gzip_serving
-          puts "rack-zippy is not recommended for this version of Rails. Rails now supports serving gzipped files using its own ActionDispatch::Static middleware. Please remove rack-zippy from your app."
+          puts "[rack-zippy] rack-zippy is not supported for this version of Rails. Rails now supports serving gzipped files using its own ActionDispatch::Static middleware. It is strongly recommended you remove rack-zippy from your app and use ActionDispatch::Static in its place."
         end
       end
 
       def self.minimum_rails_version_with_gzip_serving
         4.2
+      end
+
+      config.after_initialize do
+        Rack::Zippy::Railtie.version_check
       end
 
     end
--- test/railtie_test.rb
+++ test/railtie_test.rb
@@ -2,6 +2,9 @@ require_relative 'test_helper'
 
 module Rails
   class Railtie
+    def self.config
+      OpenStruct.new(after_initialize: true)
+    end
   end
 
   def self.version
@@ -15,6 +18,10 @@ module Rack
   module Zippy
     class RailtieTest <  TestCase
 
+      def teardown
+        Rack::Zippy::Railtie.skip_version_check = false
+      end
+
       def test_no_remove_zippy_recommendation_for_rails_versions_below_4_2
         with_rails_version('4.1.99') do
           assert_silent { Rack::Zippy::Railtie.version_check }
@@ -25,26 +32,19 @@ module Rack
       versions.each do |version|
         test "remove zippy recommendation printed for Rails versions #{version}" do
             with_rails_version(version) do
-              assert_output("rack-zippy is not recommended for this version of Rails. Rails now supports serving gzipped files using its own ActionDispatch::Static middleware. Please remove rack-zippy from your app.\n") { Rack::Zippy::Railtie.version_check }
+              assert_output("[rack-zippy] rack-zippy is not supported for this version of Rails. Rails now supports serving gzipped files using its own ActionDispatch::Static middleware. It is strongly recommended you remove rack-zippy from your app and use ActionDispatch::Static in its place.\n") { Rack::Zippy::Railtie.version_check }
             end
         end
       end
 
-
       def test_config_silences_remove_zippy_recommendation_for_rails_versions_4_2_and_above
-        # Look at Railties and delay version check to later in startup
-        # def Rack::Zippy.skip_rails_version_check = ->() { true }
-        # Rack::Zippy.skip_rails_version_check
         versions = ['4.2.0', '5.0.0', '10.0.0']
         versions.each do |version|
           with_rails_version(version) do
+            Rack::Zippy::Railtie.skip_version_check = true
             assert_silent { Rack::Zippy::Railtie.version_check }
           end
         end
-      ensure
-        # class << Rack::Zippy
-        #   remove_method :skip_rails_version_check
-        # end
       end
 
       private
